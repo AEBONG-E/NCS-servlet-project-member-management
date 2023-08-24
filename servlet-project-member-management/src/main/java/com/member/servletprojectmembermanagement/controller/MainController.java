126c8e7717259f94ae64a89519521ccc85fdd778
Seong-Hoon-Lim <hooney200@gmail.com>
2023-08-24 11:12:51 +0900

    #20 - 메인 컨트롤러 수정
    애플리케이션 최초 실행 되고 회원 가입 페이지 이동시 url 에 jsessionid 값이 생기는 버그 발생으로 루트 경로 진입 시 회원 아이디와 회원 이름 외 다른 세션은 초기화 하는 로직 추가

--- servlet-project-member-management/src/main/java/com/member/servletprojectmembermanagement/controller/MainController.java
+++ servlet-project-member-management/src/main/java/com/member/servletprojectmembermanagement/controller/MainController.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Log4j2
@@ -39,8 +40,33 @@ public class MainController extends HttpServlet {
         switch (command) {
             case "/":
                 log.info("MemberController: welcome");
+                HttpSession session = req.getSession(false); // false를 전달하여 세션이 없으면 null을 반환하도록 합니다.
+
+                // 아이디와 이름 세션 값을 임시 변수에 백업
+                Object sessionMemberId = null;
+                Object sessionMemberName = null;
+                if (session != null) {
+                    sessionMemberId = session.getAttribute("sessionMemberId");
+                    sessionMemberName = session.getAttribute("sessionMemberName");
+                }
+
+                // 모든 세션을 초기화
+                if (session != null) {
+                    session.invalidate();
+                }
+
+                // 새 세션을 만들고 아이디와 이름 세션만 복원
+                session = req.getSession(true);
+                if (sessionMemberId != null) {
+                    session.setAttribute("sessionMemberId", sessionMemberId);
+                }
+                if (sessionMemberName != null) {
+                    session.setAttribute("sessionMemberName", sessionMemberName);
+                }
+
                 req.getRequestDispatcher("/main/welcome.jsp").forward(req, resp);
                 break;
         }
     }
 }
+
